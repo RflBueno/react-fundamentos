0323a91d9f74d2d52a8116cc311123de20c51e23
Rafael Bueno <rafael.bueno.c@hotmail.com>
2025-05-16 23:56:30 -0300

    fix last commit success

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -3,21 +3,22 @@ import React, { useState, createContext } from 'react';
 
 export const ThemeContext = createContext('dark');
 
-export function ThemeProvider(props){
+export function ThemeProvider(props) {
     const [theme, setTheme] = useState('dark');
 
     function handleToggleTheme() {
         setTheme((prevState) => prevState === 'dark' ? 'light' : 'dark');
     }
 
-    return(
-        <ThemeContext.Provider>
+    return (
+        <ThemeContext.Provider
             value={{
                 theme,
                 onToggleTheme: handleToggleTheme,
             }}
+        >
             {props.children}
-        </ThemeContext.Provider> 
-                
+        </ThemeContext.Provider>
+
     )
 }
